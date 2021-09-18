ce8b6cb0957cfc35efab8999ece76a032e75582c
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-09-18 12:33:20 +1000

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="cartorio",
-    version="1.1.1",
+    version="1.1.2",
     description="Cartorio: A library for logging",
     long_description=README,
     long_description_content_type="text/markdown",
