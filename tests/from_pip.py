52bb9aaa3130998f8f3e7b28c479bf075e16e975
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-09-19 16:29:27 +1000

    add comments

--- tests/from_pip.py
+++ tests/from_pip.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 from cartorio.log import fun, log
 
-logger = log(filename=Path(__file__).resolve().stem, logs_path=Path(__file__).resolve().parent)
+# Tests installation from pip
 
+# Test instantiation of log file
+logger = log(filename=Path(__file__).resolve().stem, logs_path=Path(__file__).resolve().parent)
 
 @fun
 def divide(num1, num2):
@@ -14,9 +16,11 @@ def divide(num1, num2):
 def multiply(num1, num2):
     return num1 * num2
 
+# Test if entry and exit log messages are correct
 multiply(10, 1)
 try:
     divide(10, 0)
 
 except ZeroDivisionError as e:
+    # Test if raised error is correct
     print(sys.exc_info())
